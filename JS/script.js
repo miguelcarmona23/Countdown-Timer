8da3c5b3b91b9804ebecb801d4e5e2614768193e
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-14 10:56:57 -0500

    Added function formatTime

--- JS/script.js
+++ JS/script.js
@@ -67,3 +67,14 @@ function startTimer() {
         }
     }, 1000);
 }
+
+function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
+    }
+
+    return `${minutes}:${seconds}`;
+}
