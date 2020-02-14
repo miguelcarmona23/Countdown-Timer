2ef15de356106143c29c9d8b3d2d2bfae25a4cd9
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-14 11:30:52 -0500

    JS file format

--- JS/script.js
+++ JS/script.js
@@ -24,25 +24,25 @@ let remainingPathColor = COLOR_CODES.info.color;
 
 document.getElementById("app").innerHTML = `
 <div class="base-timer">
-<svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
     <g class="base-timer__circle">
-        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></>
-        <path
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
         id="base-timer-path-remaining"
         stroke-dasharray="283"
         class="base-timer__path-remaining ${remainingPathColor}"
         d="
-            M 50, 50
-            m -45, 0
-            a 45,45 0 1,0 90,0
-            a 45,45 0 1,0 -90,0
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
         "
-        ></path>
+      ></path>
     </g>
-</svg>
-<span id="base-timer-label" class="base-timer__label">${formatTime(
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${formatTime(
     timeLeft
-)}</span>
+  )}</span>
 </div>
 `;
 
@@ -56,9 +56,15 @@ function startTimer() {
     timerInterval = setInterval(() => {
         timePassed = timePassed += 1;
         timeLeft = TIME_LIMIT - timePassed;
-        document.getElementById("base-timer-label").innerHTML = formatTime(timeLeft);
-
+        document.getElementById("base-timer-label").innerHTML = formatTime(
+            timeLeft
+        );
         setCircleDasharray();
+        setRemainingPathColor(timeLeft);
+
+        if (timeLeft === 0) {
+            onTimesUp();
+        }
     }, 1000);
 }
 
@@ -99,8 +105,8 @@ function calculateTimeFraction() {
 
 function setCircleDasharray() {
     const circleDasharray = `${(
-      calculateTimeFraction() * FULL_DASH_ARRAY
-    ).toFixed(0)} 283`;
+    calculateTimeFraction() * FULL_DASH_ARRAY
+  ).toFixed(0)} 283`;
     document
         .getElementById("base-timer-path-remaining")
         .setAttribute("stroke-dasharray", circleDasharray);
