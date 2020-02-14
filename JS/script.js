6d5c0c3e084be88595e94093d3228c6dcc619a58
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-14 10:51:33 -0500

    Added const COLOR_CODES

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,17 @@
 const FULL_DASH_ARRAY = 283;
 const WARNING_THRESHOLD = 10;
 const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+    info: {
+        color: "green"
+    },
+    warning: {
+        color: "orange",
+        threshold: WARNING_THRESHOLD
+    },
+    alert: {
+        color: "red",
+        threshold: ALERT_THRESHOLD
+    }
+};
